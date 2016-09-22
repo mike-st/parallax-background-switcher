33a89539342322ecee306fa67b584ef371478f6b
Mike Stevens <mike.stevens@vubiz.com>
2016-09-21 21:23:21 -0400

    Fixed on iPad and over bg image height fix

--- js/parallax-background-switcher.js
+++ js/parallax-background-switcher.js
@@ -35,7 +35,6 @@ define([
 			this.$blockElements = {};
 			this.$backgrounds = {};
 			this.callbacks = {};
-			$window = $(window); // Cache the Window object
 
 			for (var i = 0, l = this._blockModels.length; i < l; i++) {
 				var blockModel = this._blockModels[i];				
@@ -57,7 +56,7 @@ define([
 
 	            this.$blockElements[id].on("onscreen", this.callbacks[id]);
 
-	            $blockElement.addClass('background-switcher-block').css({'background-image': 'url('+options.src+')','height': 'auto','width': '100%','position': 'relative','box-shadow': '0px 0px 40px rgba(0,0,0,0.5)','border-top': '3px solid #FFFFFF','background-repeat': 'no-repeat','background-attachment': 'fixed'}).attr('data-speed', i*30); //INSERTED
+	            $blockElement.addClass('background-switcher-block').css({'background-image': 'url('+options.src+')','height': 'auto','width': '100%','position': 'relative','box-shadow': '0px 0px 40px rgba(0,0,0,0.5)','border-top': '3px solid #FFFFFF','background-repeat': 'no-repeat','background-attachment': 'fixed'}); //INSERTED
 
 	            var $backGround = $('<div class="background-switcher-background" style="background-image: url('+options.src+');"></div>');
 				this.$backgroundContainer.prepend($backGround);
@@ -70,8 +69,8 @@ define([
 
 			this._activeId = this._firstId;
 			
-	    if (thebgoptions === 'animation') { // INSERTED
-		this.$('.background-switcher-block').removeAttr( 'style' );
+			if (thebgoptions === 'animation') { // INSERTED
+				this.$('.background-switcher-block').removeAttr( 'style' );
                 this.showBackground();
             } else if (thebgoptions === 'parallax') {// INSERTED
                 this.showParallax();// INSERTED
@@ -149,22 +148,6 @@ define([
 			$('body').addClass('fix-parallax-edge-ie');
 
 
-			//SCROLLING EFFECT
-			$window = $(window);
-                
-				$('.block').each(function(){
-				 	var $bgobj = $(this); 
-				                
-				  	$(window).scroll(function() {
-										
-					var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-					
-					var coords = '50% '+ yPos + 'px' ;
-
-					$bgobj.css({ backgroundPosition: coords });
-					
-					}); 
-			 });
 		},
 
 		onRemove: function () {
@@ -172,10 +155,10 @@ define([
 				this.$blockElements[id].off("onscreen", this.callbacks[id]);
 			}
 			this.$blockElements = null;
-			this.$backgroundContainer = null;
+			//this.$backgroundContainer = null;
 			this.$backgrounds = null;
-			this._blockModels = null;
-			this._blockModelsIndexed = null;
+			//this._blockModels = null;
+			//this._blockModelsIndexed = null;
 			this._onBlockInview = null;
 		}
 
