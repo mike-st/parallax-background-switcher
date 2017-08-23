3d93a0513442394a354d5ea0788554cc65712edc
Mike Stevens <mesgraphix@gmail.com>
2017-08-23 13:30:42 -0400

    Removed top border on images

--- js/parallax-background-switcher.js
+++ js/parallax-background-switcher.js
@@ -57,9 +57,9 @@ define([
 	            this.$blockElements[id].on("onscreen", this.callbacks[id]);
 
 	            if (options.src == ""){
-	            	$blockElement.addClass('background-switcher-block').css({'background-image': 'url('+options.src+')','height': 'auto','width': '100%','position': 'relative','box-shadow': '0px 0px 40px rgba(0,0,0,0.0)','border-top': '0px solid #FFFFFF','background-repeat': 'no-repeat','background-attachment': 'fixed'}); //INSERTED
+	            	$blockElement.addClass('background-switcher-block').css({'background-image': 'url('+options.src+')','height': 'auto','width': '100%','position': 'relative','background-repeat': 'no-repeat','background-attachment': 'fixed'}); //INSERTED
 			    }else{
-					$blockElement.addClass('background-switcher-block').css({'background-image': 'url('+options.src+')','height': 'auto','width': '100%','position': 'relative','box-shadow': '0px 0px 40px rgba(0,0,0,0.5)','border-top': '3px solid #FFFFFF','background-repeat': 'no-repeat','background-attachment': 'fixed'}); //INSERTED
+					$blockElement.addClass('background-switcher-block').css({'background-image': 'url('+options.src+')','height': 'auto','width': '100%','position': 'relative','background-repeat': 'no-repeat','background-attachment': 'fixed'}); //INSERTED
 			    }
 
 			    var $backGround = $('<div class="background-switcher-background" style="background-image: url('+options.src+');"></div>');
